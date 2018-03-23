a5eb3b8959d85bec3060ff066858bce3175de0fa
Saket Choudhary <saketkc@gmail.com>
2018-03-22 19:41:02 -0700

    update config

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,28 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Saket Choudhary'
 SITENAME = u'Piddling Pertinent'
+SITESUBTITLE = 'trivially relevant'
+SITEDESCRIPTION = 'Musings'
 SITEURL = 'http://saket-choudhary.me/pertinent-blog'
 
 PATH = 'content'
+BROWSER_COLOR = '#333333'
+PYGMENTS_STYLE = 'monokai'
+
+ROBOTS = 'index, follow'
+
 
 TIMEZONE = 'America/Los_Angeles'
 
-DEFAULT_LANG = u'en'
+i18n_templates_lang = 'en'
+default_lang = 'en'
+og_locale = 'en_us'
+locale = 'en_us'
+
+date_formats = {
+    'en': '%b %d, %y',
+}
+
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
@@ -21,13 +36,21 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+
 # Blogroll
 #LINKS = (('Pelican', 'http://getpelican.com/'),
 #         ('Python.org', 'http://python.org/'),
 #         ('Jinja2', 'http://jinja.pocoo.org/'),
 #         ('You can modify those links in your config file', '#'),)
-LINKS = ()
-THEME = 'pelican-themes/pelican-blue'
+LINKS = [('Probability Screening Solutions', 'http://www.saket-choudhary.me/usc-math-505A-screening-solutions/'),
+         ('Stats A Screening Solutions', 'http://www.saket-choudhary.me/usc-math-541A-screening-solutions/'),
+         ('Stats B Screening Solutions', 'http://www.saket-choudhary.me/usc-math-541B-screening-solutions/'),
+         ('Royal Statistical Society Diploma Solutions', 'http://www.saket-choudhary.me/rss-graduate-diploma-solutions')]
+
+THEME = 'pelican-themes/Flex'
 # Social widget
 #SOCIAL = (('You can add links in your config file', '#'),
 #         ('Another social link', '#'),)
@@ -43,14 +66,27 @@ LOAD_CONTENT_CACHE = False
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 PLUGIN_PATHS = ['../pelican-plugins', './plugins/']
-PLUGINS = ['assets', 'sitemap', 'gravatar', 'render_math', 'ipynb.markup']
+PLUGINS = ['assets', 'sitemap', 'gravatar', 'render_math', 'ipynb.markup', 'post_stats', 'i18n_subsites']
 GOOGLE_ANALYTICS = 'UA-55540107-1'
+COPYRIGHT_YEAR = 2018
+
+MARKDOWN = {
+    'extension_configs': {
+                'markdown.extensions.codehilite': {'css_class': 'codehilite'},
+                'markdown.extensions.extra': {},
+                'markdown.extensions.meta': {},
+
+    },
+        'output_format': 'html5',
+
+}
+
+
+DEFAULT_PAGINATION = 10
+
+jinja_environment = {'extensions': ['jinja2.ext.i18n']}
 #GOSQUARED_SITENAME = 'GSN-123456-A'
-MENUITEMS = [('Home', 'http://saket-choudhary.me/pertinent-blog'), ('Github', 'https://github.com/saketkc/pertinent-blog/tree/master/content'),
-             ('Probability Screening Solutions', 'http://www.saket-choudhary.me/usc-math-505A-screening-solutions/'),
-             ('Stats A Screening Solutions', 'http://www.saket-choudhary.me/usc-math-541A-screening-solutions/'),
-             ('Stats B Screening Solutions', 'http://www.saket-choudhary.me/usc-math-541B-screening-solutions/'),
-             ('Royal Statistical Society Diploma Solutions', 'http://www.saket-choudhary.me/rss-graduate-diploma-solutions')]
+MENUITEMS = [('Home', 'http://saket-choudhary.me/pertinent-blog'), ('Github', 'https://github.com/saketkc/pertinent-blog/tree/master/content'),]
 GITHUB_URL = 'https://github.com/saketkc'
 TWITTER_USERNAME = 'saketkc'
 
@@ -75,4 +111,4 @@ SIDEBAR_DIGEST = ''
 FAVICON = 'url-to-favicon'
 
 DISPLAY_PAGES_ON_MENU = True
-
+IPYNB_IGNORE_CSS = True
